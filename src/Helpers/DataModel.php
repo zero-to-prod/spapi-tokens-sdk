21c82d17ea03e6239d04d2f629c8140d7ad50c59
david_smith <david_smith@sweetwater.com>
2025-02-04 10:32:13 -0500

    feat/replace-info Created `src/Helpers/DataModel.php`.

---
+++ src/Helpers/DataModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zerotoprod\SpapiTokensSdk\Helpers;
+
+use Zerotoprod\Transformable\Transformable;
+
+class DataModel
+{
+    use \Zerotoprod\DataModel\DataModel;
+    use Transformable;
+}
