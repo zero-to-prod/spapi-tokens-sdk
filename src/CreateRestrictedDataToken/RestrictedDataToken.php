57f50d21c3f1d3dca328f565239e2c2945dafcb4
david_smith <david_smith@sweetwater.com>
2025-02-04 08:56:52 -0500

    feat/sdk Created `src/CreateRestrictedDataToken/RestrictedDataToken.php`.

---
+++ src/CreateRestrictedDataToken/RestrictedDataToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zerotoprod\SpapiTokensSdk\CreateRestrictedDataToken;
+
+use Zerotoprod\DataModel\DataModel;
+
+/**
+ * The response schema for the createRestrictedDataToken operation.
+ */
+class RestrictedDataToken
+{
+    use DataModel;
+
+    /**
+     * A Restricted Data Token (RDT). This is a short-lived access token that authorizes calls to restricted operations. Pass this value with the x-amz-access-token header when making subsequent calls to these restricted resources.
+     *
+     * @see $restrictedDataToken
+     */
+    public const restrictedDataToken = 'restrictedDataToken';
+    /**
+     * The lifetime of the Restricted Data Token, in seconds.
+     *
+     * @see $expiresIn
+     */
+    public const expiresIn = 'expiresIn';
+    /** A Restricted Data Token (RDT). This is a short-lived access token that authorizes calls to restricted operations. Pass this value with the x-amz-access-token header when making subsequent calls to these restricted resources. */
+    public readonly string $restrictedDataToken;
+    /** The lifetime of the Restricted Data Token, in seconds. */
+    public readonly int $expiresIn;
+}
