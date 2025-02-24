8bf9b053400885762ddd0c41fff3b1f10e65bb59
David Smith <dasmith@zero-to-prod.com>
2025-02-24 17:34:15 -0500

    dependabot/composer/zero-to-prod/spapi-tokens-tw-1.0or-tw-2.0 Updated `src/CreateRestrictedDataToken/RestrictedDataToken.php`.

--- src/CreateRestrictedDataToken/RestrictedDataToken.php
+++ src/CreateRestrictedDataToken/RestrictedDataToken.php
@@ -2,11 +2,12 @@
 
 namespace Zerotoprod\SpapiTokensSdk\CreateRestrictedDataToken;
 
-
 use Zerotoprod\DataModel\DataModel;
 
 /**
  * The response schema for the createRestrictedDataToken operation.
+ *
+ * @link https://github.com/zero-to-prod/spapi-tokens-sdk
  */
 class RestrictedDataToken
 {
@@ -15,17 +16,27 @@ class RestrictedDataToken
     /**
      * A Restricted Data Token (RDT). This is a short-lived access token that authorizes calls to restricted operations. Pass this value with the x-amz-access-token header when making subsequent calls to these restricted resources.
      *
-     * @see $restrictedDataToken
+     * @see  $restrictedDataToken
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
      */
     public const restrictedDataToken = 'restrictedDataToken';
     /**
      * The lifetime of the Restricted Data Token, in seconds.
      *
-     * @see $expiresIn
+     * @see  $expiresIn
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
      */
     public const expiresIn = 'expiresIn';
-    /** A Restricted Data Token (RDT). This is a short-lived access token that authorizes calls to restricted operations. Pass this value with the x-amz-access-token header when making subsequent calls to these restricted resources. */
+    /**
+     * A Restricted Data Token (RDT). This is a short-lived access token that authorizes calls to restricted operations. Pass this value with the x-amz-access-token header when making subsequent calls to these restricted resources.
+     *
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public readonly string $restrictedDataToken;
-    /** The lifetime of the Restricted Data Token, in seconds. */
+    /**
+     * The lifetime of the Restricted Data Token, in seconds.
+     *
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public readonly int $expiresIn;
 }
