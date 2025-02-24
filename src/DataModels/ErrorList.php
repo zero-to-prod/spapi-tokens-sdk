bb156640582b6525450c2ddd58e29157cd4930aa
David Smith <dasmith@zero-to-prod.com>
2025-02-24 17:34:15 -0500

    dependabot/composer/zero-to-prod/spapi-tokens-tw-1.0or-tw-2.0 Updated `src/DataModels/ErrorList.php`.

--- src/DataModels/ErrorList.php
+++ src/DataModels/ErrorList.php
@@ -8,15 +8,21 @@ use Zerotoprod\DataModelHelper\DataModelHelper;
 
 /**
  * A list of error responses returned when a request is unsuccessful.
+ *
+ * @link https://github.com/zero-to-prod/spapi-tokens-sdk
  */
 class ErrorList
 {
     use DataModel;
 
-    /** @see $errors */
+    /**
+     * @see  $errors
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public const errors = 'errors';
     /**
      * @var array<int|string, Error>
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
      */
     #[Describe(['cast' => [DataModelHelper::class, 'mapOf'], 'type' => Error::class])]
     public readonly array $errors;
