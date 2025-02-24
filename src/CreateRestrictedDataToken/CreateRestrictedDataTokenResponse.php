b7adbe60f0e50f8ba416424f9a7cf0252ac308c2
David Smith <dasmith@zero-to-prod.com>
2025-02-24 17:34:15 -0500

    dependabot/composer/zero-to-prod/spapi-tokens-tw-1.0or-tw-2.0 Updated `src/CreateRestrictedDataToken/CreateRestrictedDataTokenResponse.php`.

--- src/CreateRestrictedDataToken/CreateRestrictedDataTokenResponse.php
+++ src/CreateRestrictedDataToken/CreateRestrictedDataTokenResponse.php
@@ -9,23 +9,52 @@ use Zerotoprod\SpapiTokensSdk\DataModels\ErrorList;
 
 /**
  * The response schema for the createRestrictedDataToken operation.
+ *
+ * @link https://github.com/zero-to-prod/spapi-tokens-sdk
  */
 class CreateRestrictedDataTokenResponse
 {
     use DataModel;
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public const info = 'info';
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public Info $info;
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public const error = 'error';
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public ?string $error = null;
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public const headers = 'headers';
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public RestrictedDataTokenResponseHeaders $headers;
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public const response = 'response';
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public RestrictedDataToken|ErrorList $response;
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-tokens-sdk
+     */
     public static function fromResponse(array $response): self
     {
         return self::from([
